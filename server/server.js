be2b74c7860833ac78a2bc3d1a76001ff32ab0f9
Daniel Vazquez <104696010+dvazjr@users.noreply.github.com>
2023-08-26 20:02:51 -0700

    added env to different directory, and changed up server side

--- server/server.js
+++ server/server.js
@@ -1,49 +1,45 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import express from 'express';
 import nodemailer from 'nodemailer';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import express from 'express';
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Server is running.');
-});
+module.exports = async (req, res) => {
+  if (req.method === 'POST') {
+    try {
+      const { name, email, subject, message } = req.body;
 
-app.post('/api/send', async (req, res) => {
-  try {
-    const { name, email, subject, message } = req.body;
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        port: 465,
+        secure: true,
+        auth: {
+          user: process.env.EMAIL,
+          pass: process.env.PASSWORD
+        }
+      });
 
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      port: 465,
-      secure: true, 
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD
-      }
-    });
+      const mailOptions = {
+        from: email,
+        to: 'dvazdev@gmail.com',
+        subject: `Portfolio Form Submission: ${subject}`,
+        text: `From: ${name}\nEmail: ${email}\nMessage: ${message}`
+      };
 
-    const mailOptions = {
-      from: email,
-      to: 'dvazdev@gmail.com',
-      subject: `Portfolio Form Submission: ${subject}`,
-      text: `From: ${name}\nEmail: ${email}\nMessage: ${message}`
-    };
+      await transporter.sendMail(mailOptions);
+      res.status(200).json({ message: 'Email Sent' });
 
-    await transporter.sendMail(mailOptions);
-    res.status(200).json({ message: 'Email Sent' });
-
-  } catch (error) {
-    console.error("Email sending failed:", error);
-    res.status(500).json({ message: 'Failed to send email', error: error.message });
+    } catch (error) {
+      console.error("Email sending failed:", error);
+      res.status(500).json({ message: 'Failed to send email', error: error.message });
+    }
+  } else {
+    res.status(405).end(); // Method not allowed
   }
-});
-
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000/');
-});
+};
