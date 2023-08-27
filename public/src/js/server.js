36efff380d024e760e967a711f793a821a29ba7f
Daniel Vazquez <104696010+dvazjr@users.noreply.github.com>
2023-08-26 22:10:49 -0700

    updates

--- public/src/js/server.js
+++ public/src/js/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-module.exports = async (req, res) => {
+export default async (req, res) => {
   if (req.method === 'POST') {
     try {
       const { name, email, subject, message } = req.body;
