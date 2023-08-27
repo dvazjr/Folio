5580b571688ad8cc54d5943d566d41b3ac111ceb
Daniel Vazquez <104696010+dvazjr@users.noreply.github.com>
2023-08-26 21:20:58 -0700

    update client

--- public/src/js/client.js
+++ public/src/js/client.js
@@ -1,36 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
-
   const env = document.querySelector('meta[name="env"]').getAttribute('content');
   const apiUrl =
     env === "production"
       ? "https://www.danielvazquez.dev/api/send"
       : "http://localhost:3000/send";
+      
+  const form = document.getElementById("contactForm");
 
-    const form = document.getElementById("contactForm");
-  
-    form.addEventListener("submit", function(event) {
-      event.preventDefault();
-  
-      const formData = new FormData(form);
-      let jsonObject = {};
-      formData.forEach((value, key) => {
-        jsonObject[key] = value;
-      });
-  
-      fetch(apiUrl, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(jsonObject),
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log("Success:", data);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    let jsonObject = {};
+    formData.forEach((value, key) => {
+      jsonObject[key] = value;
+    });
+
+    fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(jsonObject),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log("Success:", data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
     });
   });
-  
+});
